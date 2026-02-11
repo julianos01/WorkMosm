d3ffd57224d7479327a231f96045c8663e0d4ed3
marti <marti@DESKTOP-RHU7RAR>
2026-02-11 13:50:09 -0500

    Agregar archivos de proyecto.

---
+++ Application/Interfaces/Security/ITokenGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Security
+{
+    public interface ITokenGenerator
+    {
+        string GenerateToken(User user);
+    }
+}
---
+++ Domain/Ports/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Ports
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAsync(string email);
+        Task AddAsync(User user);
+        Task<User?> GetByIdAsync(int id);
+        Task UpdateAsync(User user);
+    }
+}
